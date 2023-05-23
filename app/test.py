dcc5c3e48799453a11fd70a84a5f29680e594700
yui kurozumi <saleserv50+assist@gmail.com>
2023-05-23 17:44:15 +0900

    add dry run file

--- app/test.py
+++ app/test.py
@@ -6,27 +6,30 @@ import concurrent.futures
 
 openai.api_key = os.getenv("OPENAI_API_KEY")  # OpenAI APIキーを設定
 MODEL = os.getenv("OPENAI_MODEL")  # GPT-3のモデルIDを設定
-RATE_LIMIT = 3000 # 1分あたりのリクエスト数の上限
-SLEEP_TIME = 60  # スリープする秒数（60秒 = 1分）
-LEN_TOKENS_RATE_LIMIT = 250000  # 1分あたりのトークン数の上限
+RATE_LIMIT = 15 # 1分あたりのリクエスト数の上限
+SLEEP_TIME = 4  # スリープする秒数（60秒 = 1分）
+LEN_TOKENS_RATE_LIMIT = 200  # 1分あたりのトークン数の上限
 STOP = ["\n"]
-MAX_TOKENS = 100
+MAX_TOKENS = 20
 CSV_FILE_NAME = "prompts.csv" # input directoryにあるCSVファイルの名前を指定
 RESULT_CSV_FILE_NAME = "result.csv"
-MAX_WORKERS = 10 # 並列処理のためのワーカー数を設定
+MAX_WORKERS = 7 # 並列処理のためのワーカー数を設定
 
 def query_completion(prompt):
-    response = openai.Completion.create(
-        model=MODEL,
-        prompt=prompt,
-        max_tokens=MAX_TOKENS,
-        stop=STOP
-    )
-    return response.choices[0].text.strip()
+    time.sleep(1)
+    return prompt + "completion"
+    # response = openai.Completion.create(
+    #     model=MODEL,
+    #     prompt=prompt,
+    #     max_tokens=MAX_TOKENS,
+    #     stop=STOP
+    # )
+    # return response.choices[0].text.strip()
 
 def process_prompts(prompts):
     # 並列処理のためのワーカー数を設定
     num_workers = min(len(prompts), MAX_WORKERS)
+
     start_time = time.time()
     start_time4token = time.time()
     completed_count = 0
@@ -49,49 +52,60 @@ def process_prompts(prompts):
                     print("Completion for prompt:", prompt)
                     print("Result:", completion)
                     print("completed_count:", completed_count)
-                    writer.writerow([prompt, completion])  # 結果をCSVファイルに書き込む
+                    # writer.writerow([prompt, completion])  # 結果をCSVファイルに書き込む
                 except Exception as exc:
                     # エラーハンドリング
                     print(f"Error processing prompt: {prompt}, {exc}")
 
                 completed_count += 1
-                elapsed_time = time.time() - start_time
-
+                # print("completion:", completion)
+                # print("len_token:", len(completion))
                 sum_len_tokens += len(completion)
+                # print("sum_len_tokens:", sum_len_tokens)
+                elapsed_time = time.time() - start_time
                 elapsed_time4token = time.time() - start_time4token
+
+                writer.writerow([elapsed_time, elapsed_time4token, completed_count])
 
                 if completed_count % RATE_LIMIT == 0 and elapsed_time < SLEEP_TIME:
                     # Rate limitを考慮してスリープする
                     sleep_time = SLEEP_TIME - elapsed_time
-                    print(f"Sleeping for {sleep_time} seconds to respect rate limit...")
+                    msg = f"Sleeping for {sleep_time} seconds to respect rate limit... now completed_count: {completed_count}"
+                    print(msg)
                     time.sleep(sleep_time)
                     start_time = time.time()
+                    writer.writerow(['sleep by request', msg])  # 結果をCSVファイルに書き込む
                 elif (
                     sum_len_tokens + MAX_TOKENS >= LEN_TOKENS_RATE_LIMIT
                     and elapsed_time4token < SLEEP_TIME
                 ):
                     # トークン数の上限に達したらスリープする
                     sleep_time = SLEEP_TIME - elapsed_time4token
-                    print(f"Reached token rate limit of {LEN_TOKENS_RATE_LIMIT} tokens per minute. Sleeping for {sleep_time} seconds...")
+                    msg = f"Reached token rate limit of {LEN_TOKENS_RATE_LIMIT} tokens per minute. Sleeping for {sleep_time} seconds..."
+                    print(msg)
                     time.sleep(sleep_time)
                     start_time4token = time.time()
                     sum_len_tokens = 0
+                    writer.writerow(['sleep by token', msg])  # 結果をCSVファイルに書き込む
+
 
 # CSVファイルからプロンプトを読み込む
-def load_prompts_from_csv(file_path):
+def load_prompts_from_csv():
     prompts = []
-    with open(file_path, "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row:
-                prompts.append(row[0])  # CSVの各行の最初の要素をプロンプトとして追加
+    for i in range(30):
+        prompts.append("prompt" + str(i))
+    # with open(file_path, "r") as file:
+    #     reader = csv.reader(file)
+    #     for row in reader:
+    #         if row:
+    #             prompts.append(row[0])  # CSVの各行の最初の要素をプロンプトとして追加
     return prompts
 
 if __name__ == "__main__":
     # CSVファイルのパスを指定してプロンプトをロードして実行
-    input_dir = os.path.join(os.path.dirname(__file__)) + '/input/'
-    csv_file_path = input_dir + CSV_FILE_NAME  # CSVファイルのパスを適宜変更
-    prompts = load_prompts_from_csv(csv_file_path)
+    # input_dir = os.path.join(os.path.dirname(__file__)) + '/input/'
+    # csv_file_path = input_dir + CSV_FILE_NAME  # CSVファイルのパスを適宜変更
+    prompts = load_prompts_from_csv()
     start = time.time()
     process_prompts(prompts)
     total = time.time() - start
